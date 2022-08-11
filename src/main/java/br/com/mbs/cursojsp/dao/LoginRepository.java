7af872e54475fdfe4839abd707aa11a4024885eb
mauricio-sponton <sponton.lol@gmail.com>
2022-08-10 23:37:13 -0300

    login repository

---
+++ src/main/java/br/com/mbs/cursojsp/dao/LoginRepository.java
@@ -0,0 +1,36 @@
+package br.com.mbs.cursojsp.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import br.com.mbs.cursojsp.connection.SingleConnection;
+import br.com.mbs.cursojsp.model.Login;
+
+public class LoginRepository {
+
+	private Connection connection;
+	
+	public LoginRepository() {
+		connection = SingleConnection.getConnection();
+	}
+	
+	public boolean validarAutenticacao(Login login) throws SQLException {
+		
+		String sql = "select * from login where login = ? and senha = ?";
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		statement.setString(1, login.getLogin());
+		statement.setString(2, login.getSenha());
+		
+		ResultSet resultSet = statement.executeQuery();
+		
+		if(resultSet.next()) {
+			return true;
+		}
+		
+		return false;
+	}
+}
--- src/main/java/br/com/mbs/cursojsp/servlets/ServletLogin.java
+++ src/main/java/br/com/mbs/cursojsp/servlets/ServletLogin.java
@@ -2,6 +2,7 @@ package br.com.mbs.cursojsp.servlets;
 
 import java.io.IOException;
 
+import br.com.mbs.cursojsp.dao.LoginRepository;
 import br.com.mbs.cursojsp.model.Login;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -10,9 +11,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/principal/ServletLogin"})
+@WebServlet(urlPatterns = { "/principal/ServletLogin" })
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	private LoginRepository loginRepository = new LoginRepository();
 
 	public ServletLogin() {
 
@@ -29,26 +32,31 @@ public class ServletLogin extends HttpServlet {
 		String senha = request.getParameter("senha");
 		String url = request.getParameter("url");
 
-		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+		try {
 
-			var loginModel = new Login();
-			loginModel.setLogin(login);
-			loginModel.setSenha(senha);
+			if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
 
-			if (loginModel.getLogin().equalsIgnoreCase("admin") && loginModel.getSenha().equalsIgnoreCase("admin")) {
-				request.getSession().setAttribute("usuario", loginModel.getLogin());
-				
-				if(url == null || url.equals("null")) {
-					url = "principal/principal.jsp";
+				var loginModel = new Login();
+				loginModel.setLogin(login);
+				loginModel.setSenha(senha);
+
+				if (loginRepository.validarAutenticacao(loginModel)) {
+					request.getSession().setAttribute("usuario", loginModel.getLogin());
+
+					if (url == null || url.equals("null")) {
+						url = "principal/principal.jsp";
+					}
+
+					RequestDispatcher redirecionar = request.getRequestDispatcher(url);
+					redirecionar.forward(request, response);
+				} else {
+					redirecionarPaginaLogin(request, response);
 				}
-
-				RequestDispatcher redirecionar = request.getRequestDispatcher(url);
-				redirecionar.forward(request, response);
 			} else {
 				redirecionarPaginaLogin(request, response);
 			}
-		} else {
-			redirecionarPaginaLogin(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 	}
